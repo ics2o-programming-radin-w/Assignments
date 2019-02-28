6a7cdb85f1281dde9ca33a38e16a0daa045aecf5
Radin Williams <radin.williams@ocsbstudent.ca>
2019-02-28 18:02:42 -0500

    Update main.lua

--- DisplayingShapesRadinW/main.lua
+++ DisplayingShapesRadinW/main.lua
@@ -15,17 +15,17 @@ display.setDefault("background", 204/255, 0/255, 204/255)
 local verticesParallelogram = {0,-200, 200,-200, 0,100, -200,100}
 local myParallelogram = display.newPolygon (250, 350, verticesParallelogram)
 myParallelogram:setFillColor (102/255, 0/255, 204/255)
-myParallelogram:setStrokeColor(0, 0, 0)
+myParallelogram:setStrokeColor(0, 1, 0)
 myParallelogram.strokeWidth = 10
-textObject = display.newText("parallelogram", 200, 550, nil, 50 )
+textObject = display.newText("Parallelogram", 200, 550, nil, 50 )
 
 --display trapazoid
 local verticesTrapazoid = {-150,-175, 100,-175, 150,0, -200,0}
 local myTrapazoid = display.newPolygon (800,400, verticesTrapazoid)
 myTrapazoid:setFillColor (255/255, 0/255, 0/255)
-myTrapazoid:setStrokeColor(0, 0, 0)
+myTrapazoid:setStrokeColor(0, 0, 1)
 myTrapazoid.strokeWidth = 10
-textObject = display.newText("Trapazoid", 200, 550, nil, 50 )
+textObject = display.newText("Trapazoid", 800, 550, nil, 50 )
 
 --display octagon
 local verticesOctagon = {-125,-250, 0,-250, 50,-175, 50,-125, 0,-50, -125,-50, -150,-125, -150,-175}
@@ -34,4 +34,4 @@ myOctagon:setFillColor (127/255, 0/255, 255/255)
 --set color of border 
 myOctagon:setStrokeColor(0, 0, 0)
 myOctagon.strokeWidth = 10
-textObject = display.newText("Octagon", 350, 600, nil, 50 )
+textObject = display.newText("Octagon", 500, 550, nil, 50 )
