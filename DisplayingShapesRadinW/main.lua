befb6930a8f38ecc12a135e2892293524a8d3535
Radin Williams <radin.williams@ocsbstudent.ca>
2019-02-28 16:02:07 -0500

    Update main.lua

--- DisplayingShapesRadinW/main.lua
+++ DisplayingShapesRadinW/main.lua
@@ -14,19 +14,24 @@ display.setDefault("background", 204/255, 0/255, 204/255)
 --display parallelogram
 local verticesParallelogram = {0,-200, 200,-200, 0,100, -200,100}
 local myParallelogram = display.newPolygon (250, 350, verticesParallelogram)
-display.setColor("parallelogram", 102/255, 0/255, 204/255)
+myParallelogram:setFillColor (102/255, 0/255, 204/255)
+myParallelogram:setStrokeColor(0, 0, 0)
+myParallelogram.strokeWidth = 10
+textObject = display.newText("parallelogram", 200, 550, nil, 50 )
 
 --display trapazoid
 local verticesTrapazoid = {-150,-175, 100,-175, 150,0, -200,0}
-local myTrapazoid = display.newPolygon (500,500, verticesTrapazoid)
-display.setDefaultColor("myTrapazoid", 255/255, 0/255, 0/255)
+local myTrapazoid = display.newPolygon (800,400, verticesTrapazoid)
+myTrapazoid:setFillColor (255/255, 0/255, 0/255)
+myTrapazoid:setStrokeColor(0, 0, 0)
+myTrapazoid.strokeWidth = 10
+textObject = display.newText("Trapazoid", 200, 550, nil, 50 )
 
 --display octagon
---<<<<<<< HEAD
-local verticesOctagon = {-250,-500, 0,-500, 100,-350, 100,-250, 0,-100, -250,-100, -300,-250, -300,-400}
-local myOctagon = display.newPolygon (512,384, verticesOctagon)
---=======
-local verticesOctagon = {-250,-500, 0,-500, 100,-350, 100,-250, 0,-100, -250,-100, -300,-250, -300,-350}
+local verticesOctagon = {-125,-250, 0,-250, 50,-175, 50,-125, 0,-50, -125,-50, -150,-125, -150,-175}
 local myOctagon = display.newPolygon (512,368, verticesOctagon)
-display.setDefaultColor("myOctagon", 127/255, 0/255, 255/255)
--->>>>>>> fdd819ed063bfc28ec5f1eba902a689d5aebed17
+myOctagon:setFillColor (127/255, 0/255, 255/255)
+--set color of border 
+myOctagon:setStrokeColor(0, 0, 0)
+myOctagon.strokeWidth = 10
+textObject = display.newText("Octagon", 350, 600, nil, 50 )
