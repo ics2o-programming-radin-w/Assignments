49928f3c0293fc1b8685c55d9bd5bd6cb9c7d35f
Radin Williams <radin.williams@ocsbstudent.ca>
2019-03-25 09:15:22 -0400

    added mathquiz

--- AnimatingImagesRadinW/main.lua
+++ AnimatingImagesRadinW/main.lua
@@ -8,7 +8,7 @@
 -- Animating Images
 -- Your code here
 --global variables
---scroll speed = 3
+scrollSpeed = 3
 display.setStatusBar(display.HiddenStatusBar)
 
 local backgroundImage = display.newImageRect("Images/space.png", 2048,1536)
@@ -19,10 +19,34 @@ local archer = display.newImageRect("Images/Archer.png", 200,300)
  archer.y = display.contentHeight/2
 
  local manic = display.newImageRect("Images/Manic_2.png", 192,256)
-
+ manic.alpha = 0
  manic.x = 500
  manic.y = 400
  
  local BartSimpsonPhoto = display.newImageRect("Images/BartSimpsonPhoto.png", 125,150)
  BartSimpsonPhoto.x = 800
  BartSimpsonPhoto.y = 400
+
+ local function moveArcher(event)
+ 	archer.x = archer.x + scrollSpeed
+ 	archer.y = archer.y + scrollSpeed
+ end
+Runtime:addEventListener("enterFrame", moveArcher)
+
+local function MoveManic(event)
+	manic.x = manic.x + scrollSpeed
+	manic.alpha = manic.alpha + 0.005
+end
+
+Runtime:addEventListener("enterFrame", MoveManic)
+
+BartSimpsonPhoto.yScale = 1
+BartSimpsonPhoto.xScale = 1
+
+local function MoveBart(event)
+	BartSimpsonPhoto.y = BartSimpsonPhoto.y + scrollSpeed
+	BartSimpsonPhoto.yScale = BartSimpsonPhoto.yScale + 0.01
+	BartSimpsonPhoto.xScale = BartSimpsonPhoto.xScale + 0.01
+end
+
+Runtime:addEventListener("enterFrame", MoveBart)
