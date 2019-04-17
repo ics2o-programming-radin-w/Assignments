fd802c86a6dcb54ad13b9b69b74eed0d7dbcf333
Radin Williams <47783896+s427716@users.noreply.github.com>
2019-04-17 08:57:53 -0400

    added day 26

--- CompanyLogoRadinW/CompanyAnimationRadinR/main.lua
+++ CompanyLogoRadinW/CompanyAnimationRadinR/main.lua
@@ -1,7 +1,11 @@
------------------------------------------------------------------------------------------
---
+--Title -Company logo----------------------------------------------------------------------------------------
+--By: RadinW
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+local companylogo = display.newImageRect("Images/Companylogo.png", 1000, 1000)
+local Companylogo.x = 0
+local Companylogo.y = display.contentHeight/3
