0f422dcd7b9aeff6409908724874300318100dcf
Radin Williams <radin.williams@ocsbstudent.ca>
2019-04-23 19:17:34 -0400

    addedcompanylogo

--- CompanyLogoRadinW/CompanyAnimationRadinR/main.lua
+++ CompanyLogoRadinW/CompanyAnimationRadinR/main.lua
@@ -6,6 +6,19 @@
 
 -- Your code here
 display.setStatusBar(display.HiddenStatusBar)
-local companylogo = display.newImageRect("Images/Companylogo.png", 1000, 1000)
-local Companylogo.x = 0
-local Companylogo.y = display.contentHeight/3
+
+local scrollspeed = 0.7
+
+
+local Companylogo = display.newImageRect("images/Companylogo.png", 192,256)
+Companylogo.alpha = 0
+Companylogo.x = 500
+Companylogo.y = 400
+
+local function MoveCompanylogo(event)
+	Companylogo.x = Companylogo.x + scrollspeed
+	Companylogo.alpha = Companylogo.alpha + 0.005
+	Companylogo:rotate(4,5)
+end
+
+Runtime:addEventListener("enterFrame", MoveCompanylogo)
